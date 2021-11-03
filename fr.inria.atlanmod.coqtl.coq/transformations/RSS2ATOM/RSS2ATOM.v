dfc179062043a58e960c435f99a0e9919d6d1faf
OscarGlo <oscarglo@free.fr>
2021-11-03 11:07:59 +0100

    Add other rules

--- fr.inria.atlanmod.coqtl.coq/transformations/RSS2ATOM/RSS2ATOM.v
+++ fr.inria.atlanmod.coqtl.coq/transformations/RSS2ATOM/RSS2ATOM.v
@@ -28,33 +28,72 @@ Instance R2AConfiguration : TransformationConfiguration :=
 Instance RSS2ATOMConfiguration : ModelingTransformationConfiguration R2AConfiguration :=
   Build_ModelingTransformationConfiguration R2AConfiguration RSSMetamodel_ModelingMetamodel_instance ATOMMetamodel_ModelingMetamodel_instance.
 
-(* Definition natToString(n: nat): string :=
-  if (n < 10)
-  then (ascii_of_nat n)
-  else (string (ascii_of_nat (n mod 10)) (natToString (n / 10))). *)
+Check elem.
 
-Definition RSS2AATOM :=
+Definition RSS2ATOM :=
   transformation [
     rule "Channel2ATOM"
     from [ChannelClass]
     to [elem [ChannelClass] ATOMClass "atom"
-      (fun index model channel => BuildATOM
-        (Channel_getTitle channel)
+      (fun i m c => BuildATOM
+        (Channel_getTitle c)
         "#index"
-        (Channel_getDescription channel)
-        (Channel_getCopyright channel)
+        (Channel_getDescription c)
+        (Channel_getCopyright c)
         "#icon"
         "#logo"
-        (Channel_getLastBuildDate channel)
+        (Channel_getLastBuildDate c)
       )
-      (* Map categories *)
-      [link [ChannelClass] ATOMClass ATOMCategoriesReference
+      [
+        (* categories <- At.category->asSet() *)
+        link [ChannelClass] ATOMClass ATOMCategoriesReference
         (fun tls i m c a =>
-          maybeBuildATOMCategories a
-            (maybeResolveAll tls m "cat" ATOM.CategoryClass
-              (maybeSingleton (Channel_getItemsObjects c m))
+          maybeBuildATOMCategories a (
+            maybeResolveAll tls m "cat" ATOM.CategoryClass
+              (maybeSingleton (maybeSingleton (Channel_getCategoryObject c m)))
+          )
+        );
+        (* TODO *)
+        (* links <- Sequence{link}.first() *)
+        (fun tls i m c a =>
+          Some (buildATOMLinks a (
+            resolveAll tls m "link" ATOMLinks
+              (singleton (singleton (Channel_getLink c)))
+          ))
+        )(*;
+        (* authors <- Sequence{auth}.first() *)
+        (fun tls i m c a =>
+          maybeBuildATOMAuthors a (
+            (maybeResolveAll tls m "auth" ATOMAuthors
+              (maybeSingleton (BuildATOMAuthors a (BuildAuthor)))
             )
-        )
+          )
+        ) *)
       ]
+    ];
+    rule "Item2Entry"
+    from [ItemClass]
+    to [elem [ItemClass] EntryClass "entry"
+      (fun i m it => BuildEntry
+        (Item_getTitle it)
+        (Item_getGuid it)
+        "#rights"
+        (Item_getComments it)
+        (Item_getPubDate it)
+        "#lastUpdate"
+      )
+      (* TODO *)
+      nil
+    ];
+    rule "Category2Category"
+    from [RSS.CategoryClass]
+    to [elem [RSS.CategoryClass] ATOM.CategoryClass "cat"
+      (fun i m c => ATOM.BuildCategory
+        "#term"
+        (Category_getDomain c)
+        (Category_getValue c)
+      )
+      (* TODO *)
+      nil
     ]
   ].
